a2d7621f02a23f7defbe5d213e44d4db990f8893
edrassimoes <edrassimoes@gmail.com>
2025-11-23 19:57:45 -0300

    feat: adicionar carga inicial de dados de teste

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_users (user_type, name, email, password, cpf) VALUES ('COMMON', 'Edras', 'edras@email.com', '123', '12345678900');
+INSERT INTO tb_users (user_type, name, email, password, cpf) VALUES ('COMMON', 'Sarah', 'sarah@email.com', '456', '98765432100');
+
+INSERT INTO tb_users (user_type, name, email, password, cnpj) VALUES ('MERCHANT', 'Loja do Lúcia', 'loja@email.com', '789', '12345678000199');
+INSERT INTO tb_users (user_type, name, email, password, cnpj) VALUES ('MERCHANT', 'Mercado', 'mercado@email.com', 'abc', '98765432000188');
