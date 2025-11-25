b8f677dc956d7c4a0a39f3924a384809ec3d6e9a
edrassimoes <edrassimoes@gmail.com>
2025-11-25 15:23:40 -0300

    refactor(resources): criar usuários comuns e lojistas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
-INSERT INTO tb_users (user_type, name, email, password, cpf) VALUES ('COMMON', 'Edras', 'edras@email.com', '123', '12345678900');
-INSERT INTO tb_users (user_type, name, email, password, cpf) VALUES ('COMMON', 'Sarah', 'sarah@email.com', '456', '98765432100');
+INSERT INTO tb_users (user_type, name, email, password, cpf) VALUES ('COMMON', 'Edras Simões', 'edras@email.com', '123456', '12345678900');
+INSERT INTO tb_users (user_type, name, email, password, cpf) VALUES ('COMMON', 'Maria Silva', 'maria@email.com', '456789', '98765432100');
+INSERT INTO tb_users (user_type, name, email, password, cpf) VALUES ('COMMON', 'João Santos', 'joao@email.com', 'senha123', '11122233344');
 
-INSERT INTO tb_users (user_type, name, email, password, cnpj) VALUES ('MERCHANT', 'Loja do Lúcia', 'loja@email.com', '789', '12345678000199');
-INSERT INTO tb_users (user_type, name, email, password, cnpj) VALUES ('MERCHANT', 'Mercado', 'mercado@email.com', 'abc', '98765432000188');
+INSERT INTO tb_users (user_type, name, email, password, cnpj) VALUES ('MERCHANT', 'Loja do João', 'loja@email.com', '789012', '12345678000199');
+INSERT INTO tb_users (user_type, name, email, password, cnpj) VALUES ('MERCHANT', 'Mercado Central', 'mercado@email.com', 'abc123', '98765432000188');
