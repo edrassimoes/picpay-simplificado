baea18f5730829fb42c347b5ca67e73fd7db1bc7
edrassimoes <edrassimoes@gmail.com>
2025-11-26 23:59:47 -0300

    feat(service): adicionar métodos de busca em WalletService

--- src/main/java/br/com/edras/picpaysimplificado/service/WalletService.java
+++ src/main/java/br/com/edras/picpaysimplificado/service/WalletService.java
@@ -1,20 +1,35 @@
 package br.com.edras.picpaysimplificado.service;
 
+import br.com.edras.picpaysimplificado.domain.User;
 import br.com.edras.picpaysimplificado.domain.Wallet;
+import br.com.edras.picpaysimplificado.exception.UserNotFoundException;
+import br.com.edras.picpaysimplificado.repository.UserRepository;
 import br.com.edras.picpaysimplificado.repository.WalletRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WalletService {
 
+    private final UserRepository userRepository;
     private WalletRepository walletRepository;
 
-    public WalletService(WalletRepository walletRepository) {
+    public WalletService(WalletRepository walletRepository, UserRepository userRepository) {
         this.walletRepository = walletRepository;
+        this.userRepository = userRepository;
     }
 
     public Wallet getWalletByUserId(Long userId) {
-        return walletRepository.findById(userId).orElse(null);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+        return user.getWallet();
     }
+
+    public Double getBalanceByUserId(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+        return user.getWallet().getBalance();
+    }
+
+    public
 
 }
