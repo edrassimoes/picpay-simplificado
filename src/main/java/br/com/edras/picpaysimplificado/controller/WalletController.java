099d7b424ec71b0e14176579db194d6089a9988e
edrassimoes <edrassimoes@gmail.com>
2025-11-28 23:55:46 -0300

    feat(controller): implementar endpoints para depósito e saque

--- src/main/java/br/com/edras/picpaysimplificado/controller/WalletController.java
+++ src/main/java/br/com/edras/picpaysimplificado/controller/WalletController.java
@@ -1,7 +1,10 @@
 package br.com.edras.picpaysimplificado.controller;
 
 import br.com.edras.picpaysimplificado.domain.Wallet;
+import br.com.edras.picpaysimplificado.dto.wallet.AmountDTO;
+import br.com.edras.picpaysimplificado.dto.wallet.WalletResponseDTO;
 import br.com.edras.picpaysimplificado.service.WalletService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,16 +12,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/wallets")
 public class WalletController {
 
-    private WalletService walletService;
+    private final WalletService walletService;
 
     public WalletController(WalletService walletService) {
         this.walletService = walletService;
     }
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<Wallet> getBalance(@PathVariable Long userId) {
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<WalletResponseDTO> getWalletByUserId(@PathVariable Long userId) {
         Wallet wallet = walletService.getWalletByUserId(userId);
-        return ResponseEntity.ok().body(wallet);
+        return ResponseEntity.ok().body(new WalletResponseDTO(wallet));
+    }
+
+    @PostMapping("/user/{userId}/deposit")
+    public ResponseEntity<WalletResponseDTO> deposit(
+            @PathVariable Long userId,
+            @Valid @RequestBody AmountDTO amount) {
+        Wallet wallet = walletService.deposit(userId, amount.getAmount());
+        return ResponseEntity.ok().body(new WalletResponseDTO(wallet));
+    }
+
+    @PostMapping("/user/{userId}/withdraw")
+    public ResponseEntity<WalletResponseDTO> withdraw(
+            @PathVariable Long userId,
+            @Valid @RequestBody AmountDTO amount) {
+        Wallet wallet = walletService.withdraw(userId, amount.getAmount());
+        return ResponseEntity.ok().body(new WalletResponseDTO(wallet));
     }
 
 }
