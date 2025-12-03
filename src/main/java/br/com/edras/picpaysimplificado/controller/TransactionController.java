21645bee311e85b417011f6d8e05892a50c40899
edrassimoes <edrassimoes@gmail.com>
2025-12-03 20:10:37 -0300

    refactor(controller): implementar TransactionRequestDTO e TransactionResponseDTO e remover endpoint delete

--- src/main/java/br/com/edras/picpaysimplificado/controller/TransactionController.java
+++ src/main/java/br/com/edras/picpaysimplificado/controller/TransactionController.java
@@ -1,12 +1,11 @@
 package br.com.edras.picpaysimplificado.controller;
 
-import br.com.edras.picpaysimplificado.domain.Transaction;
-import br.com.edras.picpaysimplificado.dto.transaction.TransactionDTO;
+import br.com.edras.picpaysimplificado.dto.transaction.TransactionRequestDTO;
+import br.com.edras.picpaysimplificado.dto.transaction.TransactionResponseDTO;
 import br.com.edras.picpaysimplificado.service.TransactionService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,27 +21,21 @@ public class TransactionController {
     }
 
     @PostMapping
-    public  ResponseEntity<TransactionDTO> createTransaction(@Valid @RequestBody Transaction transaction) {
-        TransactionDTO newTransaction = transactionService.transfer(transaction);
+    public  ResponseEntity<TransactionResponseDTO> createTransaction(@Valid @RequestBody TransactionRequestDTO dto) {
+        TransactionResponseDTO newTransaction = transactionService.transfer(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(newTransaction);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TransactionDTO> findById(@PathVariable Long id) {
-        TransactionDTO transaction = transactionService.findById(id);
+    public ResponseEntity<TransactionResponseDTO> findById(@PathVariable Long id) {
+        TransactionResponseDTO transaction = transactionService.findById(id);
         return ResponseEntity.ok().body(transaction);
     }
 
     @GetMapping("/users/{id}")
-    public ResponseEntity<List<TransactionDTO>> findTransactionsByUserId(@PathVariable Long id) {
-        List<TransactionDTO> transactions = transactionService.findTransactionsByUserId(id);
+    public ResponseEntity<List<TransactionResponseDTO>> findTransactionsByUserId(@PathVariable Long id) {
+        List<TransactionResponseDTO> transactions = transactionService.findTransactionsByUserId(id);
         return ResponseEntity.ok().body(transactions);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
-        transactionService.delete(id);
-        return ResponseEntity.noContent().build();
     }
 
 }
