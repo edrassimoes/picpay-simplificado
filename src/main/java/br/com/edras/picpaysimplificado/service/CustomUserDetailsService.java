64d57018e4c2750a5a395ff8e78b2b11fa5e9d7a
edrassimoes <edrassimoes@gmail.com>
2025-12-15 23:15:47 -0300

    feat: implementa UserDetailsService customizado

---
+++ src/main/java/br/com/edras/picpaysimplificado/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package br.com.edras.picpaysimplificado.service;
+
+import br.com.edras.picpaysimplificado.domain.User;
+import br.com.edras.picpaysimplificado.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("ROLE_USER") // Pode adicionar roles depois
+                .build();
+    }
+
+}
