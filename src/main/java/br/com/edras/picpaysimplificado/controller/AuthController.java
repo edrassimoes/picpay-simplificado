976161d3b775b037bbeb29268124c1f424fa51ee
edrassimoes <edrassimoes@gmail.com>
2025-12-15 23:13:45 -0300

    feat: adiciona endpoint de login em AuthController

---
+++ src/main/java/br/com/edras/picpaysimplificado/controller/AuthController.java
@@ -0,0 +1,29 @@
+package br.com.edras.picpaysimplificado.controller;
+
+import br.com.edras.picpaysimplificado.dto.jwt.AuthResponseDTO;
+import br.com.edras.picpaysimplificado.dto.jwt.LoginRequestDTO;
+import br.com.edras.picpaysimplificado.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody LoginRequestDTO request) {
+        AuthResponseDTO response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+}
