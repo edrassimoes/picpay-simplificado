18025345b21d5c1b933691dd541f237957351388
edrassimoes <edrassimoes@gmail.com>
2025-12-15 23:17:50 -0300

    feat: adiciona método findByEmail no UserRepository

--- src/main/java/br/com/edras/picpaysimplificado/repository/UserRepository.java
+++ src/main/java/br/com/edras/picpaysimplificado/repository/UserRepository.java
@@ -4,9 +4,12 @@ import br.com.edras.picpaysimplificado.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 
 }
