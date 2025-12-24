28861006a116836b948338a5eeb3323ff4857c51
edrassimoes <edrassimoes@gmail.com>
2025-12-23 23:54:36 -0300

    feat: adicionar dependencia oauth resource server

--- src/main/java/br/com/edras/picpaysimplificado/security/UserDetailsServiceImplementation.java
+++ src/main/java/br/com/edras/picpaysimplificado/security/UserDetailsServiceImplementation.java
@@ -1,6 +1,6 @@
 package br.com.edras.picpaysimplificado.service;
 
-import br.com.edras.picpaysimplificado.domain.User;
+import br.com.edras.picpaysimplificado.security.UserAuthenticated;
 import br.com.edras.picpaysimplificado.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,24 +8,20 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class UserDetailsServiceImplementation implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
+    public UserDetailsServiceImplementation(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
+        // Cria e retorna um usuário logado no sistema.
+        return userRepository.findByEmail(email)
+                .map(UserAuthenticated::new)
                 .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
-
-        return org.springframework.security.core.userdetails.User
-                .withUsername(user.getEmail())
-                .password(user.getPassword())
-                .authorities("ROLE_USER")
-                .build();
     }
 
 }
