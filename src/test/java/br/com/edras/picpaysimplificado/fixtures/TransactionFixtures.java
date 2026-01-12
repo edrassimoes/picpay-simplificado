af84782a2ff4f99592a45d8d14a3b7bf076f3b0f
edrassimoes <edrassimoes@gmail.com>
2026-01-12 17:59:38 -0300

    feat: adicionar fixture de Transaction para testes

---
+++ src/test/java/br/com/edras/picpaysimplificado/fixtures/TransactionFixtures.java
@@ -0,0 +1,27 @@
+package br.com.edras.picpaysimplificado.fixtures;
+
+import br.com.edras.picpaysimplificado.domain.CommonUser;
+import br.com.edras.picpaysimplificado.domain.Transaction;
+import br.com.edras.picpaysimplificado.domain.enums.TransactionStatus;
+
+import java.time.LocalDateTime;
+
+public class TransactionFixtures {
+
+    public static Transaction createTransaction() {
+        CommonUser payer = CommonUserFixtures.createValidCommonUser();
+        CommonUser payee = CommonUserFixtures.createValidCommonUser();
+        payer.setId(1L);
+        payee.setId(2L);
+
+        Transaction transaction = new Transaction();
+
+        transaction.setAmount(100.0);
+        transaction.setPayer(payer);
+        transaction.setPayee(payee);
+        transaction.setTimestamp(LocalDateTime.now());
+        transaction.setStatus(TransactionStatus.AUTHORIZED);
+        return transaction;
+    }
+
+}
