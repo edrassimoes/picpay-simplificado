3eb7ebf581bda9e3d8d1b66e7c7a4ef589011aa7
edrassimoes <edrassimoes@gmail.com>
2026-01-17 11:35:11 -0300

    fix: ignorar SpringSecurity e comparar por campos no teste

--- src/test/java/br/com/edras/picpaysimplificado/controller/UserControllerTest.java
+++ src/test/java/br/com/edras/picpaysimplificado/controller/UserControllerTest.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import org.springframework.boot.test.context.TestConfiguration;
@@ -16,13 +17,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@WebMvcTest(UserController.class) // Simula um contexto web.
+@WebMvcTest(controllers = UserController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class) // Simula um contexto web, ignorando a camada de segurança.
 @Import(UserControllerTest.TestConfig.class)
 public class UserControllerTest {
 
@@ -60,8 +63,11 @@ public class UserControllerTest {
         when(userService.createUser(any(UserRequestDTO.class))).thenReturn(userResponseDTO);
 
         mockMvc.perform(post("/users").content(objectMapper.writeValueAsString(userRequestDTO)).contentType("application/json"))
+                .andDo(print())
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$").value(userResponseDTO));
+                .andExpect(jsonPath("$.id", is(userResponseDTO.getId())))
+                .andExpect(jsonPath("$.name", is(userResponseDTO.getName())))
+                .andExpect(jsonPath("$.userType", is(userResponseDTO.getUserType().toString())));
     }
 
 }
