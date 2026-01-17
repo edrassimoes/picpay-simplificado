6f61e17df056b67a6a9e8a01b1ed5f0159f9ba40
edrassimoes <edrassimoes@gmail.com>
2026-01-17 14:24:34 -0300

    test: adicionar mais cenários de createUser em UserControllerTest

--- src/test/java/br/com/edras/picpaysimplificado/controller/UserControllerTest.java
+++ src/test/java/br/com/edras/picpaysimplificado/controller/UserControllerTest.java
@@ -1,9 +1,11 @@
 package br.com.edras.picpaysimplificado.controller;
 
 import br.com.edras.picpaysimplificado.domain.CommonUser;
+import br.com.edras.picpaysimplificado.domain.MerchantUser;
 import br.com.edras.picpaysimplificado.dto.user.UserRequestDTO;
 import br.com.edras.picpaysimplificado.dto.user.UserResponseDTO;
 import br.com.edras.picpaysimplificado.fixtures.CommonUserFixtures;
+import br.com.edras.picpaysimplificado.fixtures.MerchantUserFixtures;
 import br.com.edras.picpaysimplificado.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -17,6 +19,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -68,6 +71,108 @@ public class UserControllerTest {
                 .andExpect(jsonPath("$.id", is(userResponseDTO.getId())))
                 .andExpect(jsonPath("$.name", is(userResponseDTO.getName())))
                 .andExpect(jsonPath("$.userType", is(userResponseDTO.getUserType().toString())));
+    }
+
+    @Test
+    public void createUser_WithNullName_ReturnsBadRequest() throws Exception {
+        CommonUser user = CommonUserFixtures.createValidCommonUser();
+        user.setName(null);
+
+        UserRequestDTO userRequestDTO = new UserRequestDTO(user);
+        UserResponseDTO userResponseDTO = new UserResponseDTO(user);
+
+        when(userService.createUser(any(UserRequestDTO.class))).thenReturn(userResponseDTO);
+
+        mockMvc.perform(post("/users").content(objectMapper.writeValueAsString(userRequestDTO)).contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", containsString("Nome é obrigatório")
+                ));
+    }
+
+    @Test
+    public void createUser_WithNullEmail_ReturnsBadRequest() throws Exception {
+        CommonUser user = CommonUserFixtures.createValidCommonUser();
+        user.setEmail(null);
+
+        UserRequestDTO userRequestDTO = new UserRequestDTO(user);
+        UserResponseDTO userResponseDTO = new UserResponseDTO(user);
+
+        when(userService.createUser(any(UserRequestDTO.class))).thenReturn(userResponseDTO);
+
+        mockMvc.perform(post("/users").content(objectMapper.writeValueAsString(userRequestDTO)).contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", containsString("Email é obrigatório")
+                ));
+    }
+
+    @Test
+    public void createUser_WithInvalidEmail_ReturnsBadRequest() throws Exception {
+        CommonUser user = CommonUserFixtures.createValidCommonUser();
+        user.setEmail("e-mail inválido");
+
+        UserRequestDTO userRequestDTO = new UserRequestDTO(user);
+        UserResponseDTO userResponseDTO = new UserResponseDTO(user);
+
+        when(userService.createUser(any(UserRequestDTO.class))).thenReturn(userResponseDTO);
+
+        mockMvc.perform(post("/users").content(objectMapper.writeValueAsString(userRequestDTO)).contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", containsString("Email inválido")
+                ));
+    }
+
+    @Test
+    public void createUser_WithNullPassword_ReturnsBadRequest() throws Exception {
+        CommonUser user = CommonUserFixtures.createValidCommonUser();
+        user.setPassword(null);
+
+        UserRequestDTO userRequestDTO = new UserRequestDTO(user);
+        UserResponseDTO userResponseDTO = new UserResponseDTO(user);
+
+        when(userService.createUser(any(UserRequestDTO.class))).thenReturn(userResponseDTO);
+
+        mockMvc.perform(post("/users").content(objectMapper.writeValueAsString(userRequestDTO)).contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", containsString("Senha é obrigatória")
+                ));
+    }
+
+    @Test
+    public void createCommonUser_WithInvalidCpf_ReturnsBadRequest() throws Exception {
+        CommonUser user = CommonUserFixtures.createValidCommonUser();
+        user.setCpf("123");
+
+        UserRequestDTO userRequestDTO = new UserRequestDTO(user);
+        UserResponseDTO userResponseDTO = new UserResponseDTO(user);
+
+        when(userService.createUser(any(UserRequestDTO.class))).thenReturn(userResponseDTO);
+
+        mockMvc.perform(post("/users").content(objectMapper.writeValueAsString(userRequestDTO)).contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", containsString("CPF inválido")
+                ));
+    }
+
+    @Test
+    public void createMerchantUser_WithInvalidCnpj_ReturnsBadRequest() throws Exception {
+        MerchantUser user = MerchantUserFixtures.createValidMerchantUser();
+        user.setCnpj("123");
+
+        UserRequestDTO userRequestDTO = new UserRequestDTO(user);
+        UserResponseDTO userResponseDTO = new UserResponseDTO(user);
+
+        when(userService.createUser(any(UserRequestDTO.class))).thenReturn(userResponseDTO);
+
+        mockMvc.perform(post("/users").content(objectMapper.writeValueAsString(userRequestDTO)).contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", containsString("CNPJ inválido")
+                ));
     }
 
 }
