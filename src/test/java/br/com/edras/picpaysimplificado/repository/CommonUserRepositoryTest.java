2aebcee0ddcbab12ce0bc36fdca506a75e725e89
edrassimoes <edrassimoes@gmail.com>
2026-02-05 23:44:23 -0300

    test: adicionar o profile 'test' nas classes CommonUserRepositoryTest, MerchantUserRepositoryTest e UserRepositoryTest

--- src/test/java/br/com/edras/picpaysimplificado/repository/CommonUserRepositoryTest.java
+++ src/test/java/br/com/edras/picpaysimplificado/repository/CommonUserRepositoryTest.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@ActiveProfiles("test")
 @DataJpaTest
 public class CommonUserRepositoryTest {
 
--- src/test/java/br/com/edras/picpaysimplificado/repository/UserRepositoryTest.java
+++ src/test/java/br/com/edras/picpaysimplificado/repository/UserRepositoryTest.java
@@ -10,12 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@ActiveProfiles("test")
 @DataJpaTest
 public class UserRepositoryTest {
 
