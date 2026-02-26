0c81b1e0e93a8f2f8657f0da605afaba0c6a9c06
edrassimoes <edrassimoes@gmail.com>
2026-02-25 22:36:15 -0300

    feat(observability): adiciona o actuator e permite o acesso aos endpoints de monitoramento

--- src/main/java/br/com/edras/picpaysimplificado/security/SecurityConfig.java
+++ src/main/java/br/com/edras/picpaysimplificado/security/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
                         ).permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
